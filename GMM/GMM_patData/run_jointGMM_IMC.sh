939fb45e88f571d5630a25b66631dbac2753b2db
jordanbchilds <74244328+jordanbchilds@users.noreply.github.com>
2021-08-16 13:00:34 +0100

    Update run_jointGMM_IMC.sh

--- GMM/GMM_patData/run_jointGMM_IMC.sh
+++ GMM/GMM_patData/run_jointGMM_IMC.sh
@@ -4,6 +4,6 @@ nohup Rscript bayes_jointGMM_IMC.R OSCP &
 nohup Rscript bayes_jointGMM_IMC.R SDHA &
 nohup Rscript bayes_jointGMM_IMC.R NDUFB8 &
 nohup Rscript bayes_jointGMM_IMC.R GRIM19 &
-nohup Rscript bayes_jointGMM_IMC.R COX4+4LR &
+nohup Rscript bayes_jointGMM_IMC.R COX4+4L2 &
 nohup Rscript bayes_jointGMM_IMC.R UqCRC2 &
 nohup Rscript bayes_jointGMM_IMC.R MTCO1 &
