1db3188f7eb77312543be378dc3967cc0786bb37
jordanbchilds <74244328+jordanbchilds@users.noreply.github.com>
2021-08-18 11:30:17 +0100

    fullData prior updated

--- GMM/GMM_patData/bayes_fullData_GMM_IMC.R
+++ GMM/GMM_patData/bayes_fullData_GMM_IMC.R
@@ -382,14 +382,14 @@ time = system.time({
       
       ## PRIORS
       mu1_mean = c(mean(Xctrl), mean(Yctrl))
-      mu2_mean = mu1_mean
-      mu1_prec = solve( matrix(c(0.3,0.3,0.3,0.5), ncol=2, nrow=2, byrow=TRUE) )
+      mu2_mean = c(2,2)
+      mu1_prec = olve( matrix(c(0.3,0.2,0.2,0.5), nrow=2, ncol=2, byrow=TRUE))
       mu2_prec = solve( 2*diag(2) )
       
-      n_1 = 50
-      U_1 = matrix( c(0.4,0.4,0.4,0.5), ncol=2, nrow=2, byrow=TRUE)/n_1
-      n_2 = 20
-      U_2 = 2*diag(2)/n_2
+      n_1 = 10
+      U_1 = matrix( c(0.1,0.1,0.1,0.2), ncol=2, nrow=2, byrow=TRUE)/n_1
+      n_2 = 5
+      U_2 = 10*diag(2)/n_2
 
       
       alpha = 1
