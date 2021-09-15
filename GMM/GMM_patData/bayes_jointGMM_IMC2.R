90b4d04d6be12b1d5ab7e863da77aefae62e6bee
jordanbchilds <74244328+jordanbchilds@users.noreply.github.com>
2021-09-15 13:50:08 +0100

    Update bayes_jointGMM_IMC2.R

--- GMM/GMM_patData/bayes_jointGMM_IMC2.R
+++ GMM/GMM_patData/bayes_jointGMM_IMC2.R
@@ -303,7 +303,7 @@ inference = function(chan_pat){
     
     n_1 = 500
     U_1 = matrix(c(0.3,0.494,0.494,0.9), nrow=2,ncol=2)*n_1 # correlation of ~95% 
-    n_2 = 50
+    n_2 = 200
     U_2 = matrix(c(5,1,1,5),nrow=2,ncol=2)*n_2
     
     alpha = 1
