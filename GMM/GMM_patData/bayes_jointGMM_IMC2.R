7311f6c665640b590dcb24c83d53fe28fcec6c97
jordanbchilds <74244328+jordanbchilds@users.noreply.github.com>
2021-09-27 19:05:28 +0100

    Update bayes_jointGMM_IMC2.R
    Higher correlation in mu1 prior and fiddle factor removal

--- GMM/GMM_patData/bayes_jointGMM_IMC2.R
+++ GMM/GMM_patData/bayes_jointGMM_IMC2.R
@@ -303,8 +303,8 @@ inference = function(chan_pat){
     Npat = nrow(XY_pat)
     mutation_type = imcDat$mutation_type[(imcDat$patient_id==pat)&(imcDat$type=="mean intensity"),]
     
-    mu1_mean = 1.5*c(mean(Xctrl), mean(Yctrl))
-    mu1_prec = solve(matrix(c(0.05, 0.068, 0.068, 0.1), ncol=2, nrow=2, byrow=TRUE) ) # correlation of ~98%
+    mu1_mean = c(mean(Xctrl), mean(Yctrl))
+    mu1_prec = solve(matrix(c(0.2, 0.24, 0.24, 0.3), ncol=2, nrow=2, byrow=TRUE) ) # correlation of ~98%
     
     mu2_mean = mu1_mean 
     mu2_prec = 0.5*diag(2) 
