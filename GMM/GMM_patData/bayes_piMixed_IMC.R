e5f50e74662a3fb2ba92cab23e68e273c769ca49
jordanbchilds <74244328+jordanbchilds@users.noreply.github.com>
2021-09-28 17:17:31 +0100

    Update bayes_piMixed_IMC.R
    decrease degree of freedom for T_1
    2000 -> 1000

--- GMM/GMM_patData/bayes_piMixed_IMC.R
+++ GMM/GMM_patData/bayes_piMixed_IMC.R
@@ -285,7 +285,7 @@ inference = function(chan_pat){
     mu2_mean = mu1_mean 
     mu2_prec = 1*diag(2) 
     
-    n_1 = 2000
+    n_1 = 1000
     U_1 = matrix(c(0.2,0.339,0.339,0.6), nrow=2,ncol=2)*n_1 # correlation of ~97% 
     n_2 = 200
     U_2 = matrix(c(5,1,1,5),nrow=2,ncol=2)*n_2
