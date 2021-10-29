1c51bcdb9e7fd8be0e01ac1dfd71777ff0749e14
jordanbchilds <74244328+jordanbchilds@users.noreply.github.com>
2021-10-29 14:09:11 +0100

    Update bayes_jointGMM_IMC2.R
    increase variance of mu_1
    
    increase expectation of T_1

--- GMM/GMM_patData/bayes_jointGMM_IMC2.R
+++ GMM/GMM_patData/bayes_jointGMM_IMC2.R
@@ -302,13 +302,13 @@ inference = function(chan_pat){
     mutation_type = imcDat$mutation_type[(imcDat$patient_id==pat)&(imcDat$type=="mean intensity"),]
     
     mu1_mean = c(mean(Xctrl), mean(Yctrl))
-    mu1_prec = solve( matrix(c(0.075, 0.10, 0.10, 0.15), ncol=2, nrow=2, byrow=TRUE) ) # correlation of ~95.0%
+    mu1_prec = solve( matrix(c(0.1, 0.2, 0.2, 0.3), ncol=2, nrow=2, byrow=TRUE) ) # correlation of ~95.0%
     
     mu2_mean = mu1_mean 
     mu2_prec = 0.2*diag(2) 
     
     n_1 = 2000
-    U_1 = matrix(c(0.2,0.330,0.330,0.6), nrow=2,ncol=2)*n_1 # correlation of ~95% 
+    U_1 = matrix(c(0.2,0.335,0.335,0.6), nrow=2,ncol=2)*n_1 # correlation of ~95% 
     n_2 = 200
     U_2 = matrix(c(6,2,2,6),nrow=2,ncol=2)*n_2
 
